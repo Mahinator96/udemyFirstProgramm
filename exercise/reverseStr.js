44937fc14b78686852fac1db1b67af2f74db0c6b
LiderMahinator <pomnimenia2019@gmail.com>
2022-12-04 17:42:33 +0300

    add project less44

--- exercise/reverseStr.js
+++
@@ -1,8 +0,0 @@
-const someString = 'This is some strange string';
-
-function reverse(str) {
-  const reverseStr = str.split('').reverse().join('');
-  return reverseStr;
-}
-
-console.log(reverse(someString));
--- test.js
+++ test.js
@@ -1,70 +1,49 @@
-"use strict";
+const restorantData = {
+  menu: [
+      {
+          name: 'Salad Caesar',
+          price: '1$'
+      },
+      {
+          name: 'Pizza Diavola',
+          price: '9$'
+      },
+      {
+          name: 'Beefsteak',
+          price: '17$'
+      },
+      {
+          name: 'Napoleon',
+          price: '7$'
+      }
+  ],
+  waitors: [
+      {name: 'Alice', age: 22}, {name: 'John', age: 24}
+  ],
+  averageLunchPrice: '20$',
+  openNow: true
+};
 
-const personalMovieDB = {
-        count: 0,
-        movies: {},
-        actors: {},
-        genres: [],
-        private: false,
-        start: function () {
-          personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-        
-          while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-          }
-        },
-        rememberMyFilms: function() {
-          for (let i = 0; i < 2; i++){
-            const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
-                  b = prompt('На сколько оцените его?', '');
-          
-            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-              personalMovieDB.movies[a] = b;
-              console.log('done');
-            } else {
-              console.log('error');
-              i--;
-            }
-          }
-        },
-        detectPersonalLevel: function() {
-          if (personalMovieDB.count < 10) {
-            console.log('Просмотрено довольно мало фильмов');
-          } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log('Вы классический зритель');
-          } else if (personalMovieDB.count >= 30) {
-            console.log('Вы киноман');
-          } else {
-            console.log('Произошла ошибка');
-          }
-        },
-        showMyDB: function(hidden) {
-          if (!hidden) {
-            console.log(personalMovieDB);
-          }
-        },
-        writeYouGenres: function() {
-          for (let i = 1; i <= 3; i++) {
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
-            if (genre == null || genre == '') {
-              console.log('Вы ввели некорректные данные');
-              i--;
-            } else {
-              personalMovieDB.genres[i - 1] = genre; 
-            }
-          }
-          personalMovieDB.genres.forEach(function(item, i) {
-            console.log(`Любимый жанр ${i + 1} - это ${item}`);
-          });
-        },
-        toggleVisibleMyDB: function() {
-          if (personalMovieDB.private) {
-            personalMovieDB.private = false;
-          } else {
-            personalMovieDB.private = true;
-          }
-          // return personalMovieDB.private;
-        }
-      };
+function isOpen(prop) {
+  let answer = '';
+  prop ? answer = 'Открыто' : answer = 'Закрыто';
+  return answer;
+}
 
-      // personalMovieDB.writeYouGenres();
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+  if ((parseInt(fDish.price) + parseInt(sDish.price)) < parseInt(average)) {
+      return 'Цена ниже средней';
+  } else {
+      return 'Цена выше средней';
+  }
+}
+
+function transferWaitors(data) {
+  let man = {};
+  let copyCopy = Object.setPrototypeOf(man, data);
+
+  copyCopy.waitors[0] = {name: 'Mike', age: 32};
+  return man;
+}
+
+console.log(transferWaitors(restorantData));
